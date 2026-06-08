dc4e92d41c4aca2afc84e6977aa3a2852b9c16e7
K1 <keuvan.arjmnd@gmail.com>
2026-06-08 16:30:08 +0330

    changes

--- Ui/Program.cs
+++ Ui/Program.cs
@@ -2,14 +2,24 @@ using System.Reflection;
 using Application;
 using Application.Common.Mapping;
 using Application.Interfaces;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Domain.Database;
 using Domain.Entities.Identities;
+using Infrastructure;
 using Infrastructure.Repositories;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+// Autofac
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+
+builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
+{
+    containerBuilder.RegisterModule(new DependencyModule());
+});
 
 // DbContext
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -17,7 +27,6 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-builder.Services.AddSession();
 
 // Identity
 builder.Services.AddIdentity<User, IdentityRole<int>>(options =>
@@ -31,31 +40,37 @@ builder.Services.AddIdentity<User, IdentityRole<int>>(options =>
     .AddEntityFrameworkStores<AppDbContext>()
     .AddDefaultTokenProviders();
 
-// Session
-builder.Services.AddDistributedMemoryCache();
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-builder.Services.AddScoped<IFileService, FileService>();
+// MediatR
 builder.Services.AddMediatR(cfg =>
 {
-    cfg.RegisterServicesFromAssembly(typeof(ApplicationAssemblyReference).Assembly);
+    cfg.RegisterServicesFromAssembly(
+        typeof(ApplicationAssemblyReference).Assembly);
 });
-builder.Services.AddAutoMapper(cfg => { }, typeof(MappingProfile).Assembly);
+
+
+
+// Session
+builder.Services.AddDistributedMemoryCache();
+
 builder.Services.AddSession(options =>
 {
     options.IdleTimeout = TimeSpan.FromMinutes(30);
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
 });
+
+// Cookie
 builder.Services.ConfigureApplicationCookie(options =>
 {
     options.LoginPath = "/Auth/Login";
     options.AccessDeniedPath = "/Auth/AccessDenied";
-
     options.ExpireTimeSpan = TimeSpan.FromHours(5);
     options.SlidingExpiration = true;
 });
+
 // MVC
 builder.Services.AddControllersWithViews();
+builder.Services.AddAutoMapper(cfg => { }, typeof(MappingProfile).Assembly);
 
 var app = builder.Build();
 
